ffaf8e17844a65ac47f219731fb43d6d5859ef73
Uppah <philip.thorsen@live.dk>
2022-05-01 19:35:44 +0200

    uploadTestResult & updateVaccinationStatus

--- operations.py
+++ operations.py
@@ -38,32 +38,25 @@ def bookAppointment(patient: Patient, date: str, appointment_type: str, db: Data
         print("Patient does not exist, no appointment booked")
 
 
-def uploadTestResult(patient_id: int, date: str, db: Database):
-    # get date after date
-    date_after = datetime.datetime.strptime(
-        date, "%d/%m/%Y %H:%M") + datetime.timedelta(days=1)
-
-    # convert date_after to string
-    date_after_str = date_after.strftime("%d/%m/%Y %H:%M")
-
-    # get random result
-    result = ["paavist", "ikke paavist"][int(round(random.random()))]
-
-    # append new coronaTest to db.coronaTests
-    db.coronaTests.append(CoronaTest(patient_id, date_after_str, result))
+def uploadTestResult(patient_id: int, uploadDate: str, result: str, db: Database):
+    '''
+        patient_id {shs: shs}
+        uploadDate {shs: shs}
+        result     {shs: shs}
+        db         {⊥}
+    '''
+    db.coronaTests.append(CoronaTest(patient_id, uploadDate, result))
+    # {shs: shs} -> {shs: shs}
 
 
-def updateVaccinationStatus(patient_id: int, date: str, db: Database):
-    # get date after date
-    date_after = datetime.datetime.strptime(
-        date, "%d/%m/%Y %H:%M") + datetime.timedelta(days=1)
-
-    # convert date_after to string
-    date_after_str = date_after.strftime("%d/%m/%Y %H:%M")
-
-    # append new coronaTest to the database
-    db.coronaVaccinations.append(
-        CoronaVaccination(patient_id, date_after_str))
+def updateVaccinationStatus(patient_id: int, uploadDate: str, db: Database):
+    '''
+        patient_id {shs: shs}
+        uploadDate {shs: shs}
+        db         {⊥}
+    '''
+    db.coronaVaccinations.append(CoronaVaccination(patient_id, uploadDate))
+    # {shs: shs} -> {shs: shs}
 
 
 def retrievePatientData(patient_id_low: int, db: Database):
